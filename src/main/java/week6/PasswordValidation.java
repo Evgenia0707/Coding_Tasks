46380fd00ecd8971362f18ccac40553371a9237c
JekaPractice <4030071@gmail.com>
2024-03-14 16:35:43 -0400

    PasswordValidation added

---
+++ src/main/java/week6/PasswordValidation.java
@@ -0,0 +1,37 @@
+package week6;
+
+public class PasswordValidation {
+    public static void main(String[] args) {
+
+
+
+
+    }
+    public static boolean passwordIsValid(String password) {
+
+        boolean hasLowerCaseChar = password.matches("(.*[a-z].*)"),
+                hasUpperCaseChar = password.matches("(.*[A-Z].*)"),
+                hasDigits = password.matches("(.*[0-9].*)"),
+                hasSpecialChar = password.matches("(.*[ -/, :-@].*)");
+
+        if(password.length() >= 6 && !password.contains(" ")) {
+            if (hasLowerCaseChar && hasUpperCaseChar && hasDigits && hasSpecialChar) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+}
+/*
+1. Write a return method that can verify if a password is valid or not.
+-Requirements:
+1. Password MUST be at least have 6 characters and should not contain space
+2. PassWord should at least contain one upper-case letter
+3. PassWord should at least contain one lowercase letter
+4. Password should at least contain one special character
+5. Password should at least contain a digit
+-if all requirements above are met, the method returns true, otherwise returns  false
+ */
