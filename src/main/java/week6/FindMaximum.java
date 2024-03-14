1b4b18f17c3e10f995ba503dec22b2024afe8512
JekaPractice <4030071@gmail.com>
2024-03-14 16:34:10 -0400

    FindMaximum added

---
+++ src/main/java/week6/FindMaximum.java
@@ -0,0 +1,24 @@
+package week6;
+
+public class FindMaximum {
+    public static void main(String[] args) {
+
+        int[] arr = {99, 12, 23, 32, 44, 57, 6};
+        System.out.println(maxValue(arr));
+
+    }
+    public static int maxValue(int[] n) {
+
+        int max = Integer.MIN_VALUE;
+        for (int each : n) {
+            if (each > max) {
+                max = each;
+            }
+        }
+        return max;
+    }
+}
+/*
+Write a method that can find the maximum number from an int Array
+Ex:  int[] arr = {99, 12, 23, 32, 44, 57, 6} ==> 99
+ */
